0a4d4ddcf8b777cf0b7b04c94f1eeef1725be374
Dyadyusha Artem <artikool5@yandex.ru>
2024-07-10 10:49:28 +0300

    refactor: логика мобильного меню вынесена в отдельный файл

--- script.js
+++ script.js
@@ -2,30 +2,6 @@ import MapPage from "./pages/map.js";
 import ProfilePage from "./pages/profile.js";
 import TimerPage from "./pages/timer.js";
 
-// Mobile menu
-
-const mobileMenuBtns = document.querySelectorAll("header button");
-const mobileMenu = document.querySelector(".page-header__bottom");
-const mobileMenuBackdrop = document.querySelector(".mobile-menu__backdrop");
-
-function openMenu() {
-  mobileMenu.classList.add("mobile-menu-open");
-  mobileMenuBackdrop.addEventListener("click", closeMenu);
-}
-function closeMenu() {
-  mobileMenu.classList.remove("mobile-menu-open");
-  mobileMenuBackdrop.removeEventListener("click", closeMenu);
-}
-
-mobileMenuBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const openedMenu = document.querySelector(".mobile-menu-open");
-    if (openedMenu) {
-      closeMenu();
-    } else openMenu();
-  });
-});
-
 // Initialization
 
 const pages = {
